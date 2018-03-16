4f445f329d855b98b7595ea4649a7b763850e6be
Sarah Min <sarahmin@Sarahs-MacBook-Air.local>
2018-03-16 15:05:29 -0400

    defined variables

---
+++ main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+
+	var household, //total number of family members
+		var year2018, //government assistance in 2018, known
+			var year2019, //government assistance in 2019, unknown
+				var speakEnglish, //whether head of household speaks English
+					var ngo, // Names of NGOs
+						var monthlyPaycheck, // monthly paycheck for total household
+							var monthlyExpenses, // monthly expenses for total household
+								var monthleyAfterExpenses, // monthly total savings in household
+
+									var household = {
+										familyMembers = ["1", "2", "3", "4", "5 or more"]
+										monthlyExpenses = ["800", "950", "1000", "1100", "1200"]
+									}
+
+	var speakEnglish = {
+		answer = ["Yes", "No"]
+	}
+
+	var ngo = {
+		name = ["Help Unlimited", "New Start", "Better Tomorrow", "Home Safe", "New Home"]
+	}
+
+	var monthlyPaycheck = 1000;
+	var monthlyExpenses =
+		var monthleyAfterExpenses = monthlyPaycheck - monthlyExpenses;
+
+}); //This closes all your javascript
